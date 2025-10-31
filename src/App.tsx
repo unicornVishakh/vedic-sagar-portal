e6931947b23b56441d2a9be696081184df22eda3
Vishakh Agarwal <107237349+unicornVishakh@users.noreply.github.com>
2025-10-31 13:40:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ import Events from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
 import News from "./pages/News";
 
+// --- 1. IMPORT THE NEW PROVIDER ---
+import { AndroidBridgeProvider } from './contexts/AndroidBridgeContext';
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -24,24 +27,27 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/home" element={<Layout><Index /></Layout>} />
-          <Route path="/bhajans" element={<Layout><BhajanList /></Layout>} />
-          <Route path="/bhajan/:id" element={<Layout><BhajanDetail /></Layout>} />
-          <Route path="/festivals" element={<Layout><FestivalList /></Layout>} />
-          <Route path="/festival/:id" element={<Layout><FestivalDetail /></Layout>} />
-          <Route path="/page/:slug" element={<Layout><StaticPage /></Layout>} />
-          <Route path="/donation" element={<Layout><DonationPage /></Layout>} />
-          <Route path="/donate-form" element={<Layout><DonateFormPage /></Layout>} />
-          <Route path="/events" element={<Layout><Events /></Layout>} />
-          <Route path="/event/:id" element={<Layout><EventDetail /></Layout>} />
-          <Route path="/news" element={<Layout><News /></Layout>} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* --- 2. WRAP YOUR APP WITH THE PROVIDER --- */}
+      <AndroidBridgeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/home" element={<Layout><Index /></Layout>} />
+            <Route path="/bhajans" element={<Layout><BhajanList /></Layout>} />
+            <Route path="/bhajan/:id" element={<Layout><BhajanDetail /></Layout>} />
+            <Route path="/festivals" element={<Layout><FestivalList /></Layout>} />
+            <Route path="/festival/:id" element={<Layout><FestivalDetail /></Layout>} />
+            <Route path="/page/:slug" element={<Layout><StaticPage /></Layout>} />
+            <Route path="/donation" element={<Layout><DonationPage /></Layout>} />
+            <Route path="/donate-form" element={<Layout><DonateFormPage /></Layout>} />
+            <Route path="/events" element={<Layout><Events /></Layout>} />
+            <Route path="/event/:id" element={<Layout><EventDetail /></Layout>} />
+            <Route path="/news" element={<Layout><News /></Layout>} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AndroidBridgeProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
