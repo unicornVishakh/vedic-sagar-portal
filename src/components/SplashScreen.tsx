462a75e5b76f05c30c4b05acb4c4fe05aacccc73
roshanyadav-2109 <businessmarketing0002@gmail.com>
2026-01-09 14:19:43 +0530

    Update SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -18,7 +18,7 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
 
   // Initialize audio only once
   useEffect(() => {
-    const audio = new Audio("/assets/WhatsApp Video 2026-01-03 at 19.05.23.mp3");
+    const audio = new Audio("https://ugoimceidzwjytznhwig.supabase.co/storage/v1/object/public/assets/audio/public_assets_WhatsApp%20Video%202026-01-03%20at%2019.05.23.mp3
     audio.loop = true;
     audio.volume = 0.5;
     audioRef.current = audio;
